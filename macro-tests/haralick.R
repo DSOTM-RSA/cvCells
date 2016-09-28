e471bb972f564eb236b3519b30bd114d1e5b4af7
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2016-09-28 19:29:55 +0200

    scr: diffs between 2 species

--- macro-tests/haralick.R
+++ macro-tests/haralick.R
@@ -1,6 +1,44 @@
-format(Sys.time())
+
 
 library(EBImage)
+
+bspp.grey <- readImage("01-g-bspp.tif")
+bspp.seg <- readImage("01-s-bspp.tif")
+bspp.seg.lab = bwlabel(bspp.seg)
+
+#compute all features and trim down to relative size features
+bspp.fts = computeFeatures(bspp.seg.lab,bspp.grey)
+ind = which(bspp.fts[,6]>=10000)
+bspp.fts.trim = bspp.fts[ind,]
+
+# compute texture
+bspp.har<-computeFeatures.haralick(bspp.seg.lab,bspp.grey)
+bspp.har.trim = bspp.har[ind,]
+
+obj1<-c(bspp.fts.trim,bspp.har.trim)
+
+
+
+iacu.grey <- readImage("03-g-iacu.tif")
+iacu.seg <- readImage("03-s-iacu.tif")
+iacu.seg.lab = bwlabel(iacu.seg)
+
+display(iacu.seg.lab, title='Embryos Grey')
+
+#compute all features and trim down to relative size features
+iacu.fts = computeFeatures(iacu.seg.lab,iacu.grey)
+ind = which(iacu.fts[,6]>=10000)
+iacu.fts.trim = iacu.fts[ind,]
+
+# compute texture
+iacu.har<-computeFeatures.haralick(iacu.seg.lab,iacu.grey)
+iacu.har.trim = iacu.har[ind,]
+
+obj2<-c(iacu.fts.trim,iacu.har.trim)
+
+
+
+
 
 em.grey = readImage("embryos-grey.tif")
 display(em.grey, title='Embryos Grey')
