f511287441cef6bca51b49f872354e7cdff2088f
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2016-10-01 17:15:41 +0200

    scr: outputting feature-arrays

--- macro-tests/haralick.R
+++ macro-tests/haralick.R
@@ -1,46 +1,50 @@
 # cvCells
-# Using shape and texture features to classfiy microscopy images
+# Using shape and texture features to classify microscopy images
 
 
 # Part I - Data Management
 library(EBImage)
 
-# basic book-keeping
-refs.all <- list.files(pattern = "*-g-bspp")
-segs.all <- list.files(pattern = "*-s-bspp")
+# basic book-keeping -- list all files
+refs.all <-list.files(pattern = "*-g-bspp")
+segs.all <-list.files(pattern = "*-s-bspp")
+
+refs.all <-list.files(pattern = "*-g-iacu")
+segs.all <-list.files(pattern = "*-s-iacu")
 
 beg = 1
 end = as.numeric(length(refs.all))
 
 # short labels for meta-data
-ref <- as.character(strsplit(refs.all,".tif"))
-seg <- as.character(strsplit(segs.all,".tif"))
+ref <-as.character(strsplit(refs.all,".tif"))
+seg <-as.character(strsplit(segs.all,".tif"))
 
 
 
 # main loop
 for (i in beg:end){
   
-  # read and label images
+  # read and label segmented images
   refs.array <-readImage(refs.all[i])
   seg.array <-readImage(segs.all[i])
   seg.labelled <-bwlabel(seg.array)
   
-  # contruct holder for feature-results
-  writer.1 <- paste0("shapes",i)
+  # contruct holders for feature-results
+  writer.1 <-paste0("shapes",i)
   writer.2 <-paste0("textures",i)
   
   # compute features
-  fts.shp <- computeFeatures(seg.labelled,refs.array)
-  fts.tex <- computeFeatures.haralick(seg.labelled,refs.array)
+  fts.shp <-computeFeatures(seg.labelled,refs.array)
+  fts.tex <-computeFeatures.haralick(seg.labelled,refs.array)
+  
   #--->
   # can trim here or outside loop
   #crt = which(fts[,6]>=10000) # size criteria here
   #fts.trim = fts[crt,]
   #--->
   
-  # assigning rownames here for completeness
-  rownames(fts.shp)<-rep(ref[i],dim(fts.shp)[1])
+  # assigning source file ids to rownames for completeness
+  rownames(fts.shp) <-rep(ref[i],dim(fts.shp)[1])
   
   # use assign for each feature set
   assign(writer.1,fts.shp)
@@ -50,37 +54,48 @@ for (i in beg:end){
 
 # concenate pieces into one matrix
 rm(list=ls(pattern = "^fts"))
-pieces.shp <- Filter(function(x) is(x, "matrix"),mget(ls(pattern = "^shapes")))
-pieces.tex <- Filter(function(x) is(x, "matrix"), mget(ls(pattern= "^textures")))
+pieces.shp <-Filter(function(x) is(x, "matrix"),mget(ls(pattern = "^shapes")))
+pieces.tex <-Filter(function(x) is(x, "matrix"), mget(ls(pattern= "^textures")))
 
 # construct lists
-data.shapes<-do.call(rbind,pieces.shp)
-data.textures<-do.call(rbind,pieces.tex)
+data.shapes <-do.call(rbind,pieces.shp)
+data.textures <-do.call(rbind,pieces.tex)
 
 
 # trim data
-crt = which(data.shapes[,6]>=10000) # size criteria here
-data.shapes.trim = data.shapes[crt,] # apply to shape data
-data.textures.trim = data.textures[crt,] # apply for textures
+crt <-which(data.shapes[,6]>=10000) # size criteria here
+data.shapes.trim <-data.shapes[crt,] # apply to shape data
+data.textures.trim <-data.textures[crt,] # apply for textures
 
 # bind rows for full array of features
-array.images<-cbind(data.shapes.trim,data.textures.trim)
+array.images <-cbind(data.shapes.trim,data.textures.trim)
 
 # write out feature matrix
 write.csv(array.images,file = "array.csv",row.names = TRUE)
 
+rm(list = ls(pattern = "^shapes"))
+rm(list = ls(pattern = "^textures"))
 
-# Part II - Model Creation
+# Write out data-sets
 library(stringr)
 library(magrittr)
 
 # to DF
-array.dfs <- as.data.frame(array.images)
+array.dfs <-as.data.frame(array.images)
 
 # create a column of image-names 
-rNames <- rownames(array.dfs)
-rNames.tag<- str_sub(rNames,-4)
+rNames <-rownames(array.dfs)
+rNames.tag <-str_sub(rNames,-4)
 
 array.dfs %<>% cbind(rNames.tag,.)
+save(array.dfs,file="array.Rdata")
+
+
+# Part II - Model Creation
+
+# load in data-sets
+load("Array1.Rdata")
+load("array2.Rdata")
+join.df<-rbind(array.dfs,array.dfs.2)
 
 
